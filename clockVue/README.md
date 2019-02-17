1ddc5665bc5a1397e0a88a8d3463a8a15f426fca
dddddO <lbfdeatq@gmail.com>
2019-02-17 15:47:20 +0900

    add

--- clockVue/README.md
+++ clockVue/README.md
@@ -21,4 +21,6 @@ npm run build --report
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 ## 参考
-https://www.monster-dive.com/blog/web_creative/20180608_001789.php
+Vue.jsことはじめ：https://www.monster-dive.com/blog/web_creative/20180608_001789.php
+computed：https://qiita.com/kaorina/items/bb261a119b9f02e02c2d
+class/id 使い分け：https://wp-p.info/tpl_rep.php?cat=css-biginner&fl=r12
--- clockVue/src/App.vue
+++ clockVue/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <Clock class="clock" location="TOKYO" :diff="0"/>
+    <Clock class="clock" location="NY" :diff="-14"/>
+    <Clock class="clock" location="LA" :diff="-17"/>
+    <Clock class="clock" location="UK" :diff="-9"/>
+    <Clock class="clock" location="BRAZIL" :diff="-12"/>
+    <Clock class="clock" location="SYDNEY" :diff="1"/>
+    <Clock class="clock" location="DUBAI" :diff="-5"/>
+    <Clock class="clock" location="JOHANNESBURG" :diff="-7"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Clock from "@/components/Clock"
+ 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Clock,
+  },
 }
 </script>
 
+<style scoped>
+.clock {
+  width: 80%;
+  max-width: 500px;
+  margin: 30px auto;
+}
+</style>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  font-size: 62.5%;
+}
+ 
+body {
+  margin: 0;
+}
+ 
+p {
+  margin: 0;
 }
 </style>
