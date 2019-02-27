e95f33beebe38cdad7a9027b0eed9bb632e573f9
dddddO <lbfdeatq@gmail.com>
2019-02-28 01:34:03 +0900

    new JS

---
+++ JS/README.md
@@ -0,0 +1,6 @@
+## 参考
+https://note.mu/erukiti/n/n02217beab2ef
+SD３月号　P62~87 (P68まで)
+
+## exec command
+node <JSfile>
---
+++ JS/helllo-world.js
@@ -0,0 +1,11 @@
+console.log("HELLO WORLD");
+
+console.log(3 / 2);
+console.log(2 ** 53 == 2**53 + 1);
+console.log();
+
+// 文字列はプリミティブ(=イミュータブル)
+var str = "foobar"
+var repStr = str.replace("foo", "hoge")
+console.log(str)
+console.log(repStr)
