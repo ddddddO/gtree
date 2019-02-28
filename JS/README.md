b9baa8521070c016f85f1d479d53462029838863
dddddO <lbfdeatq@gmail.com>
2019-03-01 02:43:37 +0900

    add

--- JS/README.md
+++ JS/README.md
@@ -1,6 +1,6 @@
 ## 参考
 https://note.mu/erukiti/n/n02217beab2ef
-SD３月号　P62~87 (P68まで)
+SD３月号　P62~87 (P70半まで)
 
 ## exec command
 node 'JSfile'
--- JS/helllo-world.js
+++ JS/helllo-world.js
@@ -5,7 +5,35 @@ console.log(2 ** 53 == 2**53 + 1);
 console.log();
 
 // 文字列はプリミティブ(=イミュータブル)
-var str = "foobar"
-var repStr = str.replace("foo", "hoge")
-console.log(str)
-console.log(repStr)
+var str = "foobar";
+var repStr = str.replace("foo", "hoge");
+console.log(str);
+console.log(repStr);
+console.log();
+
+// オブジェクト(基本、連想配列。あと配列)
+var obj = {foo: 999, bar: "ああああ"};
+console.log(obj);
+console.log(obj.foo);
+console.log(obj["bar"]);
+console.log(obj.nnn);
+console.log()
+
+obj["buzz"] = "BUZZ";
+console.log(obj);
+console.log()
+
+obj.buzz = "fizz";
+console.log(obj);
+console.log()
+
+// オブジェクトの要素追加
+obj.fizz = null; 
+console.log(obj)
+console.log()
+
+var list = ["AA", 3, 000, true, {foo: "FOOOO"}];
+console.log(list);
+console.log(list[4].foo);
+console.log(list[4]["foo"]);
+console.log()
