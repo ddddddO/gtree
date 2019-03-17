c411e851da6136d691f04a694a19d123c99cc327
ddddddO <lbfdeatq@gmail.com>
2019-03-18 00:42:59 +0900

    add animal interface

---
+++ cmd/overrride/animal/animal.go
@@ -0,0 +1,23 @@
+package animal
+
+import (
+	"fmt"
+)
+
+type Creature interface {
+	Run()
+	Cry()
+}
+
+type Animal struct {
+	Name  string
+	Voice string
+}
+
+func (a *Animal) Run() {
+	fmt.Println(a.Name + "is Run")
+}
+
+func (a *Animal) Voice() {
+	fmt.Printf("%s, %s!\n", a.Voice, a.Voice)
+}
