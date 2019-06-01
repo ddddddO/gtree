89ddfa77424645683b6c3edba5a587477d88c336
ddddddO <lbfdeatq@gmail.com>
2019-06-01 14:01:36 +0900

    h2o-exec

--- cmd/README.md
+++ cmd/README.md
@@ -1,3 +1,7 @@
+## ビルド  
+・Linux raspberrypi 4.14.79-v7+ #1159 SMP Sun Nov 4 17:50:20 GMT 2018 armv7l GNU/Linux  
+　→GOOS=linux GOARCH=arm go build  
+
 ## Go 継承 vs 委譲  
 https://qiita.com/sonatard/items/2b4b70694fd680f6297c  
 
--- cmd/h2o-exec/main.go
+++ cmd/h2o-exec/main.go
@@ -14,7 +14,7 @@ func main() {
 }
 
 func startH2O() {
-	cmd := exec.Command("bash", "-c", "/home/pi/h2o/h2o -c /usr/local/etc/h2o/h2o.conf &")
+	cmd := exec.Command("bash", "-c", "sudo /home/pi/h2o/h2o -c /usr/local/etc/h2o/h2o.conf &")
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
