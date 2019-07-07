10571970d95b732ef4908abf9046d0117193b9f4
ddddddO <lbfdeatq@gmail.com>
2019-07-07 21:41:51 +0900

    add

---
+++ cmd/goru/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	s := []string{"a", "b", "c", "d", "e"}
+	ch := make(chan string)
+
+	log.Println("start")
+	for i := 0; i < len(s); i++ {
+		go func(i int) {
+			time.Sleep(time.Duration(i) * 2 * time.Second)
+			ch <- s[i]
+		}(i)
+	}
+
+	f := func() {
+		for {
+			select {
+			case c := <-ch:
+				fmt.Printf("%s desu\n", c)
+			case <-time.After(3 * time.Second):
+				fmt.Println("afafa")
+				go func() {
+					ch <- "z"
+				}()
+			}
+		}
+	}
+
+	for {
+		f()
+	}
+
+	log.Println("end")
+}
