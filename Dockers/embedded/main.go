6824fa715fc0b40227ce1ad81d126b4a96570106
ddddddO <lbfdeatq@gmail.com>
2019-07-15 19:48:45 +0900

    chan

--- Dockers/embedded/main.go
+++ Dockers/embedded/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/ddddddO/work/cmd/embedded/lib"
 )
+
+type data struct {
+	num   int
+	isErr bool
+}
 
 func main() {
 	dp := DockerParts{}
@@ -21,18 +25,34 @@ type DockerParts struct {
 func (dp DockerParts) Update(vs []interface{}) {
 	fmt.Println("DockerParts Update")
 
-	wg := &sync.WaitGroup{}
+	ch := make(chan data, len(vs))
 	for i, _ := range vs {
-		wg.Add(1)
-
-		go dp.routine(i, wg)
+		go dp.routine(i, ch)
 	}
-	wg.Wait()
+
+	for i := 0; i < len(vs); i++ {
+		d := <-ch
+		if d.isErr {
+			fmt.Printf("faild num: %d\n", d.num)
+		} else {
+			fmt.Printf("suceeded num: %d\n", d.num)
+		}
+	}
 }
 
-func (dp DockerParts) routine(v interface{}, wg *sync.WaitGroup) {
+func (dp DockerParts) routine(v interface{}, ch chan data) {
 	fmt.Println(v)
 	time.Sleep(1 * time.Second)
 
-	wg.Done()
+	num := v.(int)
+	d := data{
+		num:   num,
+		isErr: false,
+	}
+
+	if num == 2 {
+		d.isErr = true
+	}
+
+	ch <- d
 }
