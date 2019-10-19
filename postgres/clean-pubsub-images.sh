334ea10fd63e955bf06a39ff8a42491cb301fdd4
ddddddO <lbfdeatq@gmail.com>
2019-10-20 01:31:44 +0900

    launch pubsub

--- postgres/clean-pubsub-images.sh
+++ postgres/clean-pubsub-images.sh
@@ -2,6 +2,20 @@
 
 set -ex
 
-docker rmi postgres_pub -f
-docker rmi postgres_sub1 -f
-docker rmi postgres_sub2 -f
+function down() {
+	docker-compose down
+}
+
+function cleanup() {
+	docker rmi postgres_pub -f
+	docker rmi postgres_sub1 -f
+	docker rmi postgres_sub2 -f
+}
+
+function up() {
+	docker-compose up
+}
+
+down
+cleanup
+up
