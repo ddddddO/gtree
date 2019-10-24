a995cf1b301ab97e33fd0c6f5c818e9d0f5831c9
ddddddO <lbfdeatq@gmail.com>
2019-10-25 01:17:39 +0900

    GKEのノードにgcsの読み取り専用アクセス権を付与

--- postgres/deployments/03_terraform/main.tf
+++ postgres/deployments/03_terraform/main.tf
@@ -44,6 +44,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/devstorage.read_only", # https://www.terraform.io/docs/providers/google/r/container_cluster.html#oauth_scopes
     ]
   }
 }
