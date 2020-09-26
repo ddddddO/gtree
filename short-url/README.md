ccf6f8db6af5faf3e2f20ebdc11940ea055b57e1
ddddddO <lbfdeatq@gmail.com>
2020-09-27 00:45:56 +0900

    Add shortened url service

---
+++ short-url/README.md
@@ -0,0 +1,13 @@
+# 使い方
+## LAN内
+### 短縮URL生成サービスを提供する側のセットアップ
+1. docker-compose.yaml内の`environment`に、`- SHORTENED_URLS_SVC_HOST=http://<サービス提供するホストの任意な名前>`を記載する。
+2. `make up`を実行する。
+
+### 短縮URL生成サービスを利用する側のセットアップ
+1. /etc/hostsファイルに、以下を追記する。
+`<サービス提供するホストのローカルIP>   <サービス提供するホストの任意な名前>`
+Windowsは、[こちら](https://www.fonepaw.jp/solution/edit-windows-hosts.html)を参考にして追加する。
+
+2. `curl http://<サービス提供するホストの任意な名前>/surls/ -d 'url=<短縮したいURL>'` を実行する。
+3. curl実行結果として、短縮URLが返却される。
---
+++ short-url/cmd/api/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+)
+
+const (
+	sortenedURLsPath = "/surls/"
+)
+
+var (
+	host        string
+	redisHost   string
+	redisClient *redis.Client
+)
+
+func init() {
+	host = os.Getenv("SHORTENED_URLS_SVC_HOST")
+	if host == "" {
+		host = "http://localhost"
+	}
+
+	redisHost = os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		redisHost = "localhost:6379"
+	}
+
+	redisClient = newRedisClient()
+}
+
+func newRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     redisHost,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
+
+func main() {
+	log.Print("Start service")
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc(sortenedURLsPath, shortenedurlsHandler)
+
+	if err := http.ListenAndServe(":8888", nil); err != nil {
+		log.Fatalf("Failed: %v", err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		fmt.Fprint(w, "Bad request")
+		return
+	}
+
+	if r.Method == "GET" {
+		fmt.Fprint(w, "TODO: return shotened url request page")
+		return
+	}
+
+	fmt.Fprint(w, "Bad request")
+}
+
+func shortenedurlsHandler(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, sortenedURLsPath) {
+		fmt.Fprint(w, "Bad request")
+		return
+	}
+
+	if r.Method == "GET" {
+		sURL := strings.TrimPrefix(r.URL.Path, sortenedURLsPath)
+
+		// KVSからsURLで検索して、リダイレクト
+		var ctx = context.Background()
+		realURL, err := redisClient.Get(ctx, sURL).Result()
+		if err != nil {
+			if err == redis.Nil {
+				fmt.Fprint(w, "key does not exists")
+				return
+			}
+			fmt.Fprint(w, err)
+			return
+		}
+		http.Redirect(w, r, realURL, http.StatusSeeOther)
+		return
+	} else if r.Method == "POST" {
+		realURL := r.PostFormValue("url")
+		if realURL == "" {
+			fmt.Fprint(w, "empty url value")
+			return
+		}
+
+		// 短縮URL用の文字列を生成して、KVSに登録し、短縮URLを返却
+		uid, err := uuid.NewRandom()
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+		ust := uid.String()
+		genPath := strings.Split(ust, "-")[0]
+
+		var ctx = context.Background()
+		err = redisClient.Set(ctx, genPath, realURL, 0).Err()
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+
+		fmt.Fprint(w, host+sortenedURLsPath+genPath+"\n")
+		return
+	}
+
+	fmt.Fprint(w, "Bad request")
+}
