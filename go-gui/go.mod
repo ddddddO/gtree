31fb21af39aeed23b265166ac18b03df63b53043
ddddddO <lbfdeatq@gmail.com>
2020-11-12 02:39:36 +0900

    Add go gui

---
+++ go-gui/go.mod
@@ -0,0 +1,5 @@
+module github.com/ddddddO/work/go-gui
+
+go 1.13
+
+require fyne.io/fyne v1.4.0 // indirect
---
+++ go-gui/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	w.SetContent(widget.NewLabel("Hello, world!"))
+	w.ShowAndRun()
+}
