747bb0f59cb9f91fabff7a850f447782dc71e569
ddddddO <lbfdeatq@gmail.com>
2021-04-04 00:45:37 +0900

    Fix Goで委譲なら埋め込み

--- go/gof/adapter/adapter.go
+++ go/gof/adapter/adapter.go
@@ -6,24 +6,24 @@ import (
 
 // Adapter役(適合する側。Target役(必要とされているもの)を満たそうとする役)
 type Adapter struct {
-	adaptee *Adaptee
+	*Adaptee
 }
 
 func NewAdapter(description string) *Adapter {
 	adaptee := NewAdaptee(description)
 	return &Adapter{
-		adaptee: adaptee,
+		adaptee,
 	}
 }
 
 func (adapter *Adapter) OutputSharpFrame() {
 	fmt.Println("#############")
-	adapter.adaptee.Output()
+	adapter.Output()
 	fmt.Println("#############")
 }
 
 func (adapter *Adapter) OutputHyphenFrame() {
 	fmt.Println("-------------")
-	adapter.adaptee.Output()
+	adapter.Output()
 	fmt.Println("-------------")
 }
