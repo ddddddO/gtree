cb819f287c5de3f14c223fd5424fc8875191886e
ddddddO <lbfdeatq@gmail.com>
2021-04-14 01:42:27 +0900

    Modify factory method

--- python/gof/factory_method/character.py
+++ python/gof/factory_method/character.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+# Product役。生成されるインスタンス(Product)が持つべきインタフェースを定める抽象クラス。
+# 具体的な内容は、Concrete Product役が定める。
 class Character(metaclass=ABCMeta):
     @abstractmethod
     def attack(self):
--- python/gof/factory_method/main.py
+++ python/gof/factory_method/main.py
@@ -11,3 +11,14 @@ if __name__ == '__main__':
 
     hero_a.attack()
     hero_b.escape()
+
+    print('これまでに作られたHEROたちを呼んだ: {}'.format(hero_factory.call_heros()))
+
+    # Output:
+    # HERO alis が 誕生した！
+    # HERO bob が 誕生した！
+    # HERO alis は 攻撃した！
+    # ビッグバン
+    # メラゾーマ
+    # HERO bob は 逃げ出した！
+    # これまでに作られたHEROたちを呼んだ: ['alis', 'bob']
