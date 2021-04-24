4f8122b8820d1ac839af3a2b65abfa25a240f958
ddddddO <lbfdeatq@gmail.com>
2021-04-24 22:10:22 +0900

    Add neo4j func

--- graphql/go.mod
+++ graphql/go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/graphql-go/graphql v0.7.9 // indirect
 	github.com/graphql-go/handler v0.2.3 // indirect
+	github.com/neo4j/neo4j-go-driver/v4 v4.2.4 // indirect
 )
--- graphql/main.go
+++ graphql/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,9 +12,19 @@ import (
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 func main() {
+	// neo4j
+	dsn := "neo4j://localhost:7687"
+	driver, err := neo4j.NewDriver(dsn, neo4j.BasicAuth("username", "password", ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer driver.Close()
+
+	// graphql
 	// Schema
 	fields := graphql.Fields{
 		"hello": &graphql.Field{
@@ -47,6 +58,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/graphql", h)
 	mux.HandleFunc("/health", health())
+	mux.HandleFunc("/debugNeo4j", debug(driver))
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -76,4 +88,58 @@ func health() func(http.ResponseWriter, *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"msg":"health ok"}`))
 	}
+}
+
+func debug(driver neo4j.Driver) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		item, err := insertItem(driver)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("%+v\n", item)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("%+v", item)))
+	}
+}
+
+func insertItem(driver neo4j.Driver) (*Item, error) {
+	// Sessions are short-lived, cheap to create and NOT thread safe. Typically create one or more sessions
+	// per request in your web application. Make sure to call Close on the session when done.
+	// For multi-database support, set sessionConfig.DatabaseName to requested database
+	// Session config will default to write mode, if only reads are to be used configure session for
+	// read mode.
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+	result, err := session.WriteTransaction(createItemFn)
+	if err != nil {
+		return nil, err
+	}
+	return result.(*Item), nil
+}
+
+func createItemFn(tx neo4j.Transaction) (interface{}, error) {
+	records, err := tx.Run("CREATE (n:Item { id: $id, name: $name }) RETURN n.id, n.name", map[string]interface{}{
+		"id":   1,
+		"name": "Item 1",
+	})
+	// In face of driver native errors, make sure to return them directly.
+	// Depending on the error, the driver may try to execute the function again.
+	if err != nil {
+		return nil, err
+	}
+	record, err := records.Single()
+	if err != nil {
+		return nil, err
+	}
+	// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+	return &Item{
+		Id:   record.Values[0].(int64),
+		Name: record.Values[1].(string),
+	}, nil
+}
+
+type Item struct {
+	Id   int64
+	Name string
 }
