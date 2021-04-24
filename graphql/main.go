5354a2896910ea013e7e5001a822af5aaef77c8e
ddddddO <lbfdeatq@gmail.com>
2021-04-24 22:58:14 +0900

    Modify read neo4j in health

--- graphql/main.go
+++ graphql/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -57,7 +58,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/graphql", h)
-	mux.HandleFunc("/health", health())
+	mux.HandleFunc("/health", health(driver))
 	mux.HandleFunc("/debugNeo4j", debug(driver))
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -82,12 +83,46 @@ func main() {
 
 }
 
-func health() func(http.ResponseWriter, *http.Request) {
+func health(driver neo4j.Driver) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+
+		if err := matchItem(driver); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf(`{"msg":"%v"}`, err)))
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"msg":"health ok"}`))
 	}
+}
+
+func matchItem(driver neo4j.Driver) error {
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
+	defer session.Close()
+
+	getFn := func(tx neo4j.Transaction) (interface{}, error) {
+		result, err := tx.Run("MATCH (i:Item) RETURN i LIMIT 1", nil)
+		if err != nil {
+			return nil, err
+		}
+
+		if !result.Next() {
+			return nil, errors.New("no item")
+		}
+		if err := result.Err(); err != nil {
+			return nil, err
+		}
+		return nil, nil
+	}
+
+	_, err := session.ReadTransaction(getFn)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func debug(driver neo4j.Driver) func(http.ResponseWriter, *http.Request) {
