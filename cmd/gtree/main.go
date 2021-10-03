3366697036cb3af806509cc09fc0aff09a842e76
ddddddO <lbfdeatq@gmail.com>
2021-10-03 17:12:17 +0900

    Modify refactor

--- cmd/gtree/main.go
+++ cmd/gtree/main.go
@@ -19,42 +19,42 @@ var (
 
 func main() {
 	var (
-		isVersion                 bool
-		f                         string
-		isTwoSpaces, isFourSpaces bool
-		isWatching                bool
+		showVersion                 bool
+		mdFilepath                         string
+		twoSpaces, fourSpaces bool
+		watching                bool
 	)
-	flag.BoolVar(&isVersion, "v", false, "current gtree version")
-	flag.StringVar(&f, "f", "", "markdown file path")
-	flag.BoolVar(&isTwoSpaces, "ts", false, "for indent two spaces")
-	flag.BoolVar(&isFourSpaces, "fs", false, "for indent four spaces")
-	flag.BoolVar(&isWatching, "w", false, "watching input file")
+	flag.BoolVar(&showVersion, "v", false, "current gtree version")
+	flag.StringVar(&mdFilepath, "f", "", "markdown file path")
+	flag.BoolVar(&twoSpaces, "ts", false, "for indent two spaces")
+	flag.BoolVar(&fourSpaces, "fs", false, "for indent four spaces")
+	flag.BoolVar(&watching, "w", false, "watching input file")
 	flag.Parse()
 
-	if isVersion {
+	if showVersion {
 		fmt.Printf("gtree version %s / revision %s\n", Version, Revision)
 		return
 	}
-	if isTwoSpaces && isFourSpaces {
+	if twoSpaces && fourSpaces {
 		fmt.Errorf("%s", `choose either "ts" or "fs".`)
 		os.Exit(1)
 	}
 
-	if f == "" || f == "-" {
-		if err := execute(os.Stdout, os.Stdin, isTwoSpaces, isFourSpaces); err != nil {
+	if mdFilepath == "" || mdFilepath == "-" {
+		if err := execute(os.Stdout, os.Stdin, twoSpaces, fourSpaces); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 		return
 	}
 
-	filePath, err := filepath.Abs(f)
+	filePath, err := filepath.Abs(mdFilepath)
 	if err != nil {
 		fmt.Errorf("%+v", err)
 		os.Exit(1)
 	}
 
-	if !isWatching {
+	if !watching {
 		file, err := os.Open(filePath)
 		if err != nil {
 			fmt.Errorf("%+v", err)
@@ -62,7 +62,7 @@ func main() {
 		}
 		defer file.Close()
 
-		if err := execute(os.Stdout, file, isTwoSpaces, isFourSpaces); err != nil {
+		if err := execute(os.Stdout, file, twoSpaces, fourSpaces); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -89,18 +89,18 @@ func main() {
 			if fileInfo.ModTime() != preFileModTime {
 				preFileModTime = fileInfo.ModTime()
 
-				_ = execute(os.Stdout, file, isTwoSpaces, isFourSpaces)
+				_ = execute(os.Stdout, file, twoSpaces, fourSpaces)
 			}
 		}()
 	}
 }
 
-func execute(out io.Writer, in io.Reader, isTwoSpaces, isFourSpaces bool) error {
+func execute(out io.Writer, in io.Reader, twoSpaces, fourSpaces bool) error {
 	var err error
 	switch {
-	case isTwoSpaces:
+	case twoSpaces:
 		err = gtree.Execute(out, in, gtree.IndentTwoSpaces())
-	case isFourSpaces:
+	case fourSpaces:
 		err = gtree.Execute(out, in, gtree.IndentFourSpaces())
 	default:
 		err = gtree.Execute(out, in)
