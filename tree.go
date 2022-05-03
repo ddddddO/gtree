f297622c7518804a93fab28121c3cb5bcb78be08
ddddddO <lbfdeatq@gmail.com>
2022-05-03 12:10:46 +0900

    rename files

--- tree.go
+++ tree.go
@@ -4,44 +4,6 @@ import (
 	"io"
 )
 
-// Output outputs a tree to w with r as Markdown format input.
-func Output(w io.Writer, r io.Reader, options ...Option) error {
-	conf, err := newConfig(options...)
-	if err != nil {
-		return err
-	}
-	rg := newRootGenerator(r, conf.space)
-	roots, err := rg.generate()
-	if err != nil {
-		return err
-	}
-
-	tree := initializeTree(conf, roots)
-	if err := tree.grow(); err != nil {
-		return err
-	}
-	return tree.spread(w)
-}
-
-// Mkdir makes directories.
-func Mkdir(r io.Reader, options ...Option) error {
-	conf, err := newConfig(options...)
-	if err != nil {
-		return err
-	}
-	rg := newRootGenerator(r, conf.space)
-	roots, err := rg.generate()
-	if err != nil {
-		return err
-	}
-
-	tree := initializeTree(conf, roots)
-	if err := tree.grow(); err != nil {
-		return err
-	}
-	return tree.mkdir()
-}
-
 func initializeTree(conf *config, rs []*Node) *tree {
 	g := newGrower(conf.encode, conf.lastNodeFormat, conf.intermedialNodeFormat, conf.dryrun)
 	s := newSpreader(conf.encode)
---
+++ tree_handler.go
@@ -0,0 +1,43 @@
+package gtree
+
+import (
+	"io"
+)
+
+// Output outputs a tree to w with r as Markdown format input.
+func Output(w io.Writer, r io.Reader, options ...Option) error {
+	conf, err := newConfig(options...)
+	if err != nil {
+		return err
+	}
+	rg := newRootGenerator(r, conf.space)
+	roots, err := rg.generate()
+	if err != nil {
+		return err
+	}
+
+	tree := initializeTree(conf, roots)
+	if err := tree.grow(); err != nil {
+		return err
+	}
+	return tree.spread(w)
+}
+
+// Mkdir makes directories.
+func Mkdir(r io.Reader, options ...Option) error {
+	conf, err := newConfig(options...)
+	if err != nil {
+		return err
+	}
+	rg := newRootGenerator(r, conf.space)
+	roots, err := rg.generate()
+	if err != nil {
+		return err
+	}
+
+	tree := initializeTree(conf, roots)
+	if err := tree.grow(); err != nil {
+		return err
+	}
+	return tree.mkdir()
+}
