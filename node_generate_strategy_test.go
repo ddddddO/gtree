7a430e71f338d89c8fdb4b4730e5acf51227b8d2
ddddddO <lbfdeatq@gmail.com>
2022-05-03 13:17:07 +0900

    using map for tdd

--- node_generate_strategy_test.go
+++ node_generate_strategy_test.go
@@ -13,23 +13,23 @@ type want struct {
 const fixedIndex uint = 1
 
 func TestTabStrategy_Generate(t *testing.T) {
-	tests := []struct {
-		name string
+	// ref: https://zenn.dev/kimuson13/articles/go_table_driven_test
+	tests := map[string]struct {
 		row  string
 		want want
 	}{
-		{"root/hierarchy=1", "- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
-		{"child/hierarchy=2", "	- aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
-		{"child/hierarchy=2/tab on the way", "	- aaa	bb", want{Name: "aaa	bb", hierarchy: 2, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix space", " - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix chars", "xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/no hyphen", "xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/tab only", "			", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"root/hierarchy=1": {"- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
+		"child/hierarchy=2": {"	- aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
+		"child/hierarchy=2/tab on the way": {"	- aaa	bb", want{Name: "aaa	bb", hierarchy: 2, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix space": {" - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix chars": {"xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/no hyphen":    {"xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/tab only": {"			", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
 	}
 
-	for _, tt := range tests {
+	for name, tt := range tests {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
 			node := (*tabStrategy)(nil).generate(tt.row, fixedIndex)
@@ -47,22 +47,21 @@ func TestTabStrategy_Generate(t *testing.T) {
 }
 
 func TestTwoSpacesStrategy_Generate(t *testing.T) {
-	tests := []struct {
-		name string
+	tests := map[string]struct {
 		row  string
 		want want
 	}{
-		{"root/hierarchy=1", "- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
-		{"child/hierarchy=2", "  - aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix odd space", " - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix chars", "xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/no hyphen", "xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/spcase only", "  ", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"root/hierarchy=1":                     {"- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
+		"child/hierarchy=2":                    {"  - aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/space only":       {"  ", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
 	}
 
-	for _, tt := range tests {
+	for name, tt := range tests {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
 			node := (*twoSpacesStrategy)(nil).generate(tt.row, fixedIndex)
@@ -80,23 +79,22 @@ func TestTwoSpacesStrategy_Generate(t *testing.T) {
 }
 
 func TestFourSpacesStrategy_Generate(t *testing.T) {
-	tests := []struct {
-		name string
+	tests := map[string]struct {
 		row  string
 		want want
 	}{
-		{"root/hierarchy=1", "- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
-		{"child/hierarchy=2", "    - aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
-		{"child/hierarchy=3", "        - aaa    bb", want{Name: "aaa    bb", hierarchy: 3, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix odd space", " - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/prefix chars", "xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/no hyphen", "xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
-		{"invalid/hierarchy=0/spcase only", "    ", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"root/hierarchy=1":                     {"- aaa bb", want{Name: "aaa bb", hierarchy: 1, index: fixedIndex}},
+		"child/hierarchy=2":                    {"    - aaa bb", want{Name: "aaa bb", hierarchy: 2, index: fixedIndex}},
+		"child/hierarchy=3":                    {"        - aaa    bb", want{Name: "aaa    bb", hierarchy: 3, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
+		"invalid/hierarchy=0/space only":       {"    ", want{Name: "", hierarchy: invalidHierarchyNum, index: fixedIndex}},
 	}
 
-	for _, tt := range tests {
+	for name, tt := range tests {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
 			node := (*fourSpacesStrategy)(nil).generate(tt.row, fixedIndex)
