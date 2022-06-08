f0e090a74c8b555d1daeef655e5bd0f02bf456b5
ddddddO <lbfdeatq@gmail.com>
2022-06-09 00:40:00 +0900

    modify

--- node_generate_strategy_test.go
+++ node_generate_strategy_test.go
@@ -1,5 +1,7 @@
 package gtree
 
+// ref: https://zenn.dev/kimuson13/articles/go_table_driven_test
+
 import (
 	"testing"
 )
@@ -8,24 +10,23 @@ type want struct {
 	name      string
 	hierarchy uint
 	index     uint
+	err       error
 }
 
 const fixedIndex uint = 1
 
 func TestTabStrategy_Generate(t *testing.T) {
-	// ref: https://zenn.dev/kimuson13/articles/go_table_driven_test
 	tests := map[string]struct {
-		row     string
-		want    *want
-		wantErr error
+		row  string
+		want *want
 	}{
-		"root/hierarchy=1": {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex}, nil},
-		"child/hierarchy=2": {"	- aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex}, nil},
-		"child/hierarchy=2/tab on the way": {"	- aaa	bb", &want{name: "aaa	bb", hierarchy: 2, index: fixedIndex}, nil},
-		"invalid/hierarchy=0/prefix space": {" - aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/prefix chars": {"xx- aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/no hyphen":    {"xx aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/tab only": {"			", nil, errIncorrectFormat},
+		"root/hierarchy=1": {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
+		"child/hierarchy=2": {"	- aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
+		"child/hierarchy=2/tab on the way": {"	- aaa	bb", &want{name: "aaa	bb", hierarchy: 2, index: fixedIndex, err: nil}},
+		"invalid/hierarchy=0/prefix space": {" - aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix chars": {"xx- aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/no hyphen":    {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/tab only": {"			", &want{err: errIncorrectFormat}},
 	}
 
 	for name, tt := range tests {
@@ -34,9 +35,9 @@ func TestTabStrategy_Generate(t *testing.T) {
 			t.Parallel()
 
 			node, err := (*tabStrategy)(nil).generate(tt.row, fixedIndex)
-			if tt.wantErr != nil {
-				if err != tt.wantErr {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.wantErr)
+			if tt.want.err != nil {
+				if err != tt.want.err {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
 				}
 				return
 			}
@@ -56,16 +57,15 @@ func TestTabStrategy_Generate(t *testing.T) {
 
 func TestTwoSpacesStrategy_Generate(t *testing.T) {
 	tests := map[string]struct {
-		row     string
-		want    *want
-		wantErr error
+		row  string
+		want *want
 	}{
-		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex}, nil},
-		"child/hierarchy=2":                    {"  - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex}, nil},
-		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/space only":       {"  ", nil, errIncorrectFormat},
+		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
+		"child/hierarchy=2":                    {"  - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/space only":       {"  ", &want{err: errIncorrectFormat}},
 	}
 
 	for name, tt := range tests {
@@ -74,9 +74,9 @@ func TestTwoSpacesStrategy_Generate(t *testing.T) {
 			t.Parallel()
 
 			node, err := (*twoSpacesStrategy)(nil).generate(tt.row, fixedIndex)
-			if tt.wantErr != nil {
-				if err != tt.wantErr {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.wantErr)
+			if tt.want.err != nil {
+				if err != tt.want.err {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
 				}
 				return
 			}
@@ -96,17 +96,16 @@ func TestTwoSpacesStrategy_Generate(t *testing.T) {
 
 func TestFourSpacesStrategy_Generate(t *testing.T) {
 	tests := map[string]struct {
-		row     string
-		want    *want
-		wantErr error
+		row  string
+		want *want
 	}{
-		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex}, nil},
-		"child/hierarchy=2":                    {"    - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex}, nil},
-		"child/hierarchy=3":                    {"        - aaa    bb", &want{name: "aaa    bb", hierarchy: 3, index: fixedIndex}, nil},
-		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", nil, errIncorrectFormat},
-		"invalid/hierarchy=0/space only":       {"    ", nil, errIncorrectFormat},
+		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
+		"child/hierarchy=2":                    {"    - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
+		"child/hierarchy=3":                    {"        - aaa    bb", &want{name: "aaa    bb", hierarchy: 3, index: fixedIndex, err: nil}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/space only":       {"    ", &want{err: errIncorrectFormat}},
 	}
 
 	for name, tt := range tests {
@@ -115,9 +114,9 @@ func TestFourSpacesStrategy_Generate(t *testing.T) {
 			t.Parallel()
 
 			node, err := (*fourSpacesStrategy)(nil).generate(tt.row, fixedIndex)
-			if tt.wantErr != nil {
-				if err != tt.wantErr {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.wantErr)
+			if tt.want.err != nil {
+				if err != tt.want.err {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
 				}
 				return
 			}
