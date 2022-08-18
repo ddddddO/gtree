625cabeb569239a28f04c2627eaca6c83657899c
ddddddO <lbfdeatq@gmail.com>
2022-08-18 13:29:58 +0900

    modify html

--- cmd/gtree-wasm/main.go
+++ cmd/gtree-wasm/main.go
@@ -10,14 +10,22 @@ import (
 func gtree(this js.Value, args []js.Value) interface{} {
 	document := js.Global().Get("document")
 
+	options := []gt.Option{gt.WithIndentTwoSpaces()}
+
+	branch1Directly := document.Call("getElementById", "branch1Directly").Get("value").String()
+	branch1Indirectly := document.Call("getElementById", "branch1Indirectly").Get("value").String()
+	options = append(options, gt.WithBranchFormatLastNode(branch1Directly, branch1Indirectly))
+
+	branch2Directly := document.Call("getElementById", "branch2Directly").Get("value").String()
+	branch2Indirectly := document.Call("getElementById", "branch2Indirectly").Get("value").String()
+	options = append(options, gt.WithBranchFormatIntermedialNode(branch2Directly, branch2Indirectly))
+
 	rawInput := document.Call("getElementById", "in").Get("value").String()
-	console := js.Global().Get("console")
-	console.Call("log", rawInput)
+	// console := js.Global().Get("console")
+	// console.Call("log", rawInput)
 
 	w := &strings.Builder{}
 	r := strings.NewReader(rawInput)
-	options := []gt.Option{gt.WithIndentTwoSpaces()}
-
 	err := gt.Output(w, r, options...)
 	if err != nil {
 		js.Global().Call("alert", err.Error())
