54316544666cf031948ff44c8712777dbbd19a35
ddddddO <lbfdeatq@gmail.com>
2022-11-16 01:14:10 +0900

    show row when incorrect input format

--- node_generator_test.go
+++ node_generator_test.go
@@ -23,9 +23,9 @@ func TestGenerateTab(t *testing.T) {
 		"root/hierarchy=1":                 {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
 		"child/hierarchy=2":                {"	- aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
 		"child/hierarchy=2/tab on the way": {"	- aaa	bb", &want{name: "aaa	bb", hierarchy: 2, index: fixedIndex, err: nil}},
-		"invalid/hierarchy=0/prefix space": {" - aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/prefix chars": {"xx- aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/no hyphen":    {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix space": {" - aaa bb", &want{err: &inputFormatError{row: " - aaa bb"}}},
+		"invalid/hierarchy=0/prefix chars": {"xx- aaa bb", &want{err: &inputFormatError{row: "xx- aaa bb"}}},
+		"invalid/hierarchy=0/no hyphen":    {"xx aaa bb", &want{err: &inputFormatError{row: "xx aaa bb"}}},
 		"invalid/hierarchy=0/tab only":     {"			", nil},
 	}
 
@@ -40,8 +40,8 @@ func TestGenerateTab(t *testing.T) {
 				return
 			}
 			if tt.want.err != nil {
-				if err != tt.want.err {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
+				if e, ok := err.(*inputFormatError); !ok {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", e, tt.want.err)
 				}
 				return
 			}
@@ -66,9 +66,9 @@ func TestGenerateTwoSpaces(t *testing.T) {
 	}{
 		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
 		"child/hierarchy=2":                    {"  - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
-		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: &inputFormatError{row: " - aaa bb"}}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: &inputFormatError{row: "xx- aaa bb"}}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: &inputFormatError{row: "xx aaa bb"}}},
 		"invalid/hierarchy=0/space only":       {"  ", nil},
 	}
 
@@ -83,8 +83,8 @@ func TestGenerateTwoSpaces(t *testing.T) {
 				return
 			}
 			if tt.want.err != nil {
-				if err != tt.want.err {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
+				if e, ok := err.(*inputFormatError); !ok {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", e, tt.want.err)
 				}
 				return
 			}
@@ -110,9 +110,9 @@ func TestGenerateFourSpaces(t *testing.T) {
 		"root/hierarchy=1":                     {"- aaa bb", &want{name: "aaa bb", hierarchy: 1, index: fixedIndex, err: nil}},
 		"child/hierarchy=2":                    {"    - aaa bb", &want{name: "aaa bb", hierarchy: 2, index: fixedIndex, err: nil}},
 		"child/hierarchy=3":                    {"        - aaa    bb", &want{name: "aaa    bb", hierarchy: 3, index: fixedIndex, err: nil}},
-		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: errIncorrectFormat}},
-		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: errIncorrectFormat}},
+		"invalid/hierarchy=0/prefix odd space": {" - aaa bb", &want{err: &inputFormatError{row: " - aaa bb"}}},
+		"invalid/hierarchy=0/prefix chars":     {"xx- aaa bb", &want{err: &inputFormatError{row: "xx- aaa bb"}}},
+		"invalid/hierarchy=0/no hyphen":        {"xx aaa bb", &want{err: &inputFormatError{row: "xx aaa bb"}}},
 		"invalid/hierarchy=0/space only":       {"    ", nil},
 	}
 
@@ -127,8 +127,8 @@ func TestGenerateFourSpaces(t *testing.T) {
 				return
 			}
 			if tt.want.err != nil {
-				if err != tt.want.err {
-					t.Errorf("\ngot: \n%v\nwant: \n%v", err, tt.want.err)
+				if e, ok := err.(*inputFormatError); !ok {
+					t.Errorf("\ngot: \n%v\nwant: \n%v", e, tt.want.err)
 				}
 				return
 			}
