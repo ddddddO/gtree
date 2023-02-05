661b8336975b978d23e37d3a483f32027c8cbd40
ddddddO <lbfdeatq@gmail.com>
2023-02-05 23:21:30 +0900

    add bench

--- tree_handler_benchmark_test.go
+++ tree_handler_benchmark_test.go
@@ -128,3 +128,15 @@ func BenchmarkOutput_10000Roots(b *testing.B) {
 }
 
 var tenThousandRoots = strings.Repeat(singleRoot, 10000)
+
+func BenchmarkOutput_20000Roots(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		w := &strings.Builder{}
+		r := strings.NewReader(twentyThousandRoots)
+		if err := gtree.Output(w, r); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+var twentyThousandRoots = strings.Repeat(singleRoot, 20000)
