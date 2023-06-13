2d09e38e8cee8df8bb495e05d09800fa9744b34f
ddddddO <lbfdeatq@gmail.com>
2023-06-14 00:59:04 +0900

    nits

--- cmd/gtree/error.go
+++ cmd/gtree/error.go
@@ -4,8 +4,8 @@ import "github.com/urfave/cli/v2"
 
 const (
 	exitCodeErrOpts = iota + 1
-	exitCodeErrOutput
 	exitCodeErrOpen
+	exitCodeErrOutput
 	exitCodeErrMkdir
 )
 
@@ -13,12 +13,12 @@ func exitErrOpts(err error) cli.ExitCoder {
 	return cli.Exit(err, exitCodeErrOpts)
 }
 
-func exitErrOutput(err error) cli.ExitCoder {
-	return cli.Exit(err, exitCodeErrOutput)
-}
-
 func exitErrOpen(err error) cli.ExitCoder {
 	return cli.Exit(err, exitCodeErrOpen)
+}
+
+func exitErrOutput(err error) cli.ExitCoder {
+	return cli.Exit(err, exitCodeErrOutput)
 }
 
 func exitErrMkdir(err error) cli.ExitCoder {
