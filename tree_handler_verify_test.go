0040fcbe17dfdcf62e83827866e3e7425bfcb7cc
ddddddO <lbfdeatq@gmail.com>
2023-10-29 10:30:35 +0900

    add verify test cases

--- tree_handler_verify_test.go
+++ tree_handler_verify_test.go
@@ -1,6 +1,7 @@
 package gtree_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -35,14 +36,30 @@ func TestVerify(t *testing.T) {
 			},
 		},
 		{
+			name: "case(error/no exist root)",
+			in: in{
+				input: strings.NewReader(strings.TrimSpace(`
+- no_exist_root_dir`)),
+			},
+			out: out{
+				err: fmt.Errorf("Required paths does not exist:\n%s", "\tno_exist_root_dir"),
+			},
+		},
+		{
 			name: "case(error/strict mode/Extra paths exist and Required paths does not exist)",
 			in: in{
 				input: strings.NewReader(strings.TrimSpace(`
 - example
 	- find_pipe_programmable-gtree
 		- main.go
+		- README.md
+		- go.mod
+		- go.sum
 	- go-list_pipe_programmable-gtree
 		- main.go
+		- README.md
+		- go.mod
+		- go.sum
 	- like_cli
 		- adapter
 			- executor.go
@@ -50,11 +67,42 @@ func TestVerify(t *testing.T) {
 		- main.go
 		- xxxx
 	- programmable
-		- main.go`)),
+		- main.go
+	- README.md`)),
 				options: []gtree.Option{gtree.WithStrictVerify()},
 			},
 			out: out{
-				err: nil, // TODO: not nil
+				err: fmt.Errorf("Extra paths exist:\n%s\nRequired paths does not exist:\n%s", "\texample/noexist\n\texample/noexist/xxx", "\texample/like_cli/xxxx"),
+			},
+		},
+		{
+			name: "case(error/strict mode/specify target dir/Extra paths exist and Required paths does not exist)",
+			in: in{
+				input: strings.NewReader(strings.TrimSpace(`
+- go-list_pipe_programmable-gtree
+	- main.go
+	- README.md
+	- go.sum
+	- xxxx`)),
+				options: []gtree.Option{gtree.WithStrictVerify(), gtree.WithTargetDir("example")},
+			},
+			out: out{
+				err: fmt.Errorf("Extra paths exist:\n%s\nRequired paths does not exist:\n%s", "\texample/go-list_pipe_programmable-gtree/go.mod", "\texample/go-list_pipe_programmable-gtree/xxxx"),
+			},
+		},
+		{
+			name: "case(error/specify target dir/Required paths does not exist)",
+			in: in{
+				input: strings.NewReader(strings.TrimSpace(`
+- go-list_pipe_programmable-gtree
+	- main.go
+	- README.md
+	- go.sum
+	- xxxx`)),
+				options: []gtree.Option{gtree.WithTargetDir("example")},
+			},
+			out: out{
+				err: fmt.Errorf("Required paths does not exist:\n%s", "\texample/go-list_pipe_programmable-gtree/xxxx"),
 			},
 		},
 	}
@@ -65,11 +113,10 @@ func TestVerify(t *testing.T) {
 			t.Parallel()
 
 			gotErr := gtree.Verify(tt.in.input, tt.in.options...)
-			if gotErr != nil {
-				// TODO:
-				// if gotErr.Error() != tt.out.err.Error() {
-				// 	t.Errorf("\ngotErr: \n%v\nwantErr: \n%v", gotErr, tt.out.err)
-				// }
+			if gotErr != nil || tt.out.err != nil {
+				if gotErr.Error() != tt.out.err.Error() {
+					t.Errorf("\ngotErr: \n%s\nwantErr: \n%s", gotErr, tt.out.err)
+				}
 			}
 		})
 	}
