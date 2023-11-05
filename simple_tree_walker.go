85520a10bd00ce887e2d5f846b7162f8d8017e17
Ochi Daiki <lbfdeatq@gmail.com>
2023-11-05 10:57:14 +0900

    Impl HasChild method for WalkerNode (#254)

--- simple_tree_walker.go
+++ simple_tree_walker.go
@@ -4,11 +4,12 @@ package gtree
 
 // WalkerNode is used in user-defined function that can be executed with Walk/WalkProgrammably function.
 type WalkerNode struct {
-	name   string
-	branch string
-	row    string
-	level  uint
-	path   string
+	name     string
+	branch   string
+	row      string
+	level    uint
+	path     string
+	hasChild bool
 }
 
 // Name returns name of node in completed tree structure.
@@ -32,8 +33,14 @@ func (wn *WalkerNode) Level() uint {
 }
 
 // Path returns path of node in completed tree structure.
+// Path is the path from the root node to this node.
 func (wn *WalkerNode) Path() string {
 	return wn.path
+}
+
+// HasChild returns whether the node in completed tree structure has child nodes.
+func (wn *WalkerNode) HasChild() bool {
+	return wn.hasChild
 }
 
 func newWalkerSimple() walkerSimple {
@@ -57,11 +64,12 @@ func (dw *defaultWalkerSimple) walkNode(current *Node, cb func(*WalkerNode) erro
 		row = current.branch() + " " + current.name
 	}
 	wn := &WalkerNode{
-		name:   current.name,
-		branch: current.branch(),
-		row:    row,
-		level:  current.hierarchy,
-		path:   current.path(),
+		name:     current.name,
+		branch:   current.branch(),
+		row:      row,
+		level:    current.hierarchy,
+		path:     current.path(),
+		hasChild: current.hasChild(),
 	}
 	if err := cb(wn); err != nil {
 		return err
