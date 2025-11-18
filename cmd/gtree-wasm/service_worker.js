09ec59d2da71d7ecb9201dacdc57dbd05d2d6a61
ddddddO <lbfdeatq@gmail.com>
2025-11-19 00:38:36 +0900

    Fix gtree Web

--- cmd/gtree-wasm/service_worker.js
+++ cmd/gtree-wasm/service_worker.js
@@ -1,7 +1,7 @@
 // copied by https://laboradian.com/create-offline-site-using-sw/
 
 // TODO: ファイル変更したらCACHE_VERSIONを変えてデプロイすること
-const CACHE_VERSION = 'v1.1.14';
+const CACHE_VERSION = 'v1.1.15';
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 
 // キャッシュするファイルをセットする
