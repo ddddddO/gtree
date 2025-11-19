11e61cbcbd860b695ec7cad4c294b7a326542f0d
ddddddO <lbfdeatq@gmail.com>
2025-11-19 23:10:04 +0900

    add example

---
+++ example/n-children/go.mod
@@ -0,0 +1,14 @@
+module example.com/n-children
+
+go 1.25.2
+
+require (
+	github.com/ddddddO/gtree v1.13.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
---
+++ example/n-children/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ddddddO/gtree"
+)
+
+func main() {
+	n := 1000
+	fmt.Printf("=== %d children ===\n", n)
+
+	root := gtree.NewRoot("Parent")
+	addChildren(root, n)
+
+	if err := gtree.OutputFromRoot(os.Stdout, root); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func addChildren(root *gtree.Node, n int) {
+	for i := range n {
+		child := root.Add(fmt.Sprintf("child %d", i))
+		grandchild1 := child.Add("aaaaaaaaaa")
+		grandchild1.Add("ccccccccccccccccc")
+		grandchild2 := child.Add("bbbbbbbbbbbbbbb")
+		grandchild2.Add("ddddddddddddddddddd")
+	}
+}
